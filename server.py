f3a95b030629412925772138ae74e01386d63620
MurungaKibaara <murungakibaara@outlook.com>
2019-05-26 13:32:29 +0300

    [Deleted one file and renamed script]

--- server.py
+++ server.py
@@ -7,8 +7,6 @@ class MyTCPSocketHandler(socketserver.BaseRequestHandler):
         self.data = self.request.recv(1024).strip()
         print("{} wrote:".format(self.client_address[0]))
         print(self.data)
-        # just send back the same data, but upper-cased
-        # self.request.sendall(self.data.upper())
 
 if __name__ == "__main__":
     
